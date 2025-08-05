cec821257ef38fc199f9b0106b44ef6f3b23859f
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-08-05 19:54:00 +0100

    updated nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,54 +1,62 @@
+'use client'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import { links } from '@/lib/links'
+import { RxHamburgerMenu } from 'react-icons/rx'
+import MobileNav from './MobileNav'
 
 const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false)
+  // nav logic
+  const onMobileClick = () => {
+    setIsNavOpen(() => !isNavOpen)
+  }
   return (
-    <div className='h-[100px] shadow-2xl flex items-center justify-between px-6 relative z-40'>
-      {/* Logo/Brand */}
-      <div>
-        <Link
-          href='/'
-          className='text-2xl font-bold text-blue-600 hover:text-blue-800'
-        >
-          Next Social
-        </Link>
-      </div>
+    <nav className='h-[100px] shadow-2xl  relative z-40'>
+      <div className='max-w-[1400px] mx-auto  flex justify-between items-center h-full'>
+        {/* Logo/Brand */}
+        <div>
+          <Link
+            href='/'
+            className='text-2xl font-bold text-blue-600 hover:text-blue-800'
+          >
+            Next Social
+          </Link>
+        </div>
 
-      {/* Navigation Links */}
-      <div className='flex space-x-6'>
-        <Link
-          href='/'
-          className='text-gray-700 hover:text-blue-600 font-medium transition-colors cursor-pointer'
-        >
-          Home
-        </Link>
+        {/* Navigation Links */}
+        <div className='flex space-x-6 '>
+          <div className=' space-x-6 hidden md:flex'>
+            {links.map((item, i) => {
+              return (
+                <Link
+                  key={i}
+                  href={item.link}
+                  className='text-gray-700 hover:text-blue-600 font-medium transition-colors cursor-pointer'
+                >
+                  {item.text}
+                </Link>
+              )
+            })}
+          </div>
 
-        <Link
-          href='/auth/dashboard'
-          className='text-gray-700 hover:text-blue-600 font-medium transition-colors cursor-pointer'
-        >
-          Dash
-        </Link>
-        <Link
-          href='/posts'
-          className='text-gray-700 hover:text-blue-600 font-medium transition-colors cursor-pointer'
-        >
-          Posts
-        </Link>
-        <Link
-          href='/feed'
-          className='text-gray-700 hover:text-blue-600 font-medium transition-colors cursor-pointer '
-        >
-          Feed
-        </Link>
-        <Link
-          href='/posts/create'
-          className='bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition-colors cursor-pointer'
-        >
-          Create Post
-        </Link>
+          <button
+            onClick={onMobileClick}
+            className='bg-gradient-to-r from-blue-600 to-purple-600 w-25  justify-center items-center rounded cursor-pointer flex md:hidden'
+          >
+            <RxHamburgerMenu className='text-3xl text-white' />
+          </button>
+
+          <Link
+            href='/posts/create'
+            className='bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition-colors cursor-pointer'
+          >
+            Create Post
+          </Link>
+        </div>
       </div>
-    </div>
+      {isNavOpen && <MobileNav />}
+    </nav>
   )
 }
 
---
+++ src/lib/links.js
@@ -0,0 +1,10 @@
+const links = [
+  { link: '/', text: 'home' },
+  { link: '/auth/dashboard', text: 'dash' },
+  { link: '/posts', text: 'posts' },
+  { link: '/feed', text: 'feed' },
+]
+
+const test = ['hello']
+
+export { links, test }
